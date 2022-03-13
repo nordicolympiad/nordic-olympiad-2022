8ac66b2257f4d3dcedfa9ff0e8827d58538d67ff
Johan Sannemo <jsannemo@jsannemo.se>
2022-03-13 19:23:07 +0100

    add hipster scoring

--- hipsterjazz/input_format_validators/validator/validator.cpp
+++ hipsterjazz/input_format_validators/validator/validator.cpp
@@ -14,6 +14,8 @@ typedef vector<int> vi;
 void run() {
   int maxn = Arg("maxn", 100);
   int maxm = Arg("maxm", maxn * (maxn - 1) / 2);
+  int bipartite = Arg("bipartite", 0);
+  int maxdeg = Arg("maxdeg", maxn);
 
   int n = Int(1,maxn);
   Space();
@@ -21,13 +23,37 @@ void run() {
   Endl();
 
   set<pii> s;
+  vector<vi> G(n);
   rep(i,0,m) {
     int a = Int(1,n); Space();
     int b = Int(1,n); Endl();
+    --a; --b;
     if(s.count(pii(a,b))) die_line("duplicate edge");
     if(a == b) die_line("self_loop");
     s.emplace(a,b);
     s.emplace(b,a);
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+  trav(it, G) if (sz(it) > maxdeg) die_line("wrong maxdeg");
+  if (bipartite) {
+    vector<int> seen(n);
+    function<void(int)> dfs = [&](int at) {
+      if (seen[at]) return;
+      trav(it, G[at]) {
+        if (!seen[it]) {
+          seen[it] = 3 - seen[at];
+          dfs(it);
+        } else {
+          if (seen[at] + seen[it] != 3) die_line("not bipartite");
+        }
+      }
+    };
+    rep(i,0,n) {
+      if (seen[i]) continue;
+      seen[i] = 1;
+      dfs(i);
+    }
   }
   Eof();
 }
